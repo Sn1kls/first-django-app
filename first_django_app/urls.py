195aa13239819abf3248b09cd7126fdcf144d254
Sn1kls <macks.chigrin@gmail.com>
2024-10-22 10:39:13 +0300

    Додав модель User і згенерувати схему, налаштував інтерпритатор python 3.12, створив python directory first_django_app, бо не міг робити міграції чомусь

--- first_django_app/urls.py
+++ first_django_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for basic_django project.
+URL configuration for first-django-app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    region = models.CharField(max_length=50, blank=True)
+    house_number = models.CharField(max_length=10, blank=True)
+    street = models.CharField(max_length=100, blank=True)
+    is_doctor = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
