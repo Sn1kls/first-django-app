2ede197f719a1243bacefdfd278819c262189bdc
Sn1kls <macks.chigrin@gmail.com>
2024-10-22 15:06:19 +0300

    Поставив black як dev dependencies

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
-    email = models.EmailField( unique=True)
+    email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15)
     city = models.CharField(max_length=100)
     region = models.CharField(max_length=50, blank=True)
@@ -12,8 +13,7 @@ class User(AbstractUser):
     street = models.CharField(max_length=100, blank=True)
     is_doctor = models.BooleanField(default=False)
 
-
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'phone_number', 'city']
+    REQUIRED_FIELDS = ["first_name", "last_name", "email", "phone_number", "city"]
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
