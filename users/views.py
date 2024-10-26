13f76a4c57cf933217dd5bb83e7036bd95cc53c1
Sn1kls <macks.chigrin@gmail.com>
2024-10-26 20:19:06 +0300

    add Authentication jwt

--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": "This is a protected view!"})
+
+User = get_user_model()
+
+class RegisterView(APIView):
+    def post(self, request):
+        data = request.data
+        user = User.objects.create_user(
+            username=data['username'],
+            password=data['password'],
+            email=data['email'],
+            first_name=data.get('first_name', ''),
+            last_name=data.get('last_name', ''),
+        )
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
