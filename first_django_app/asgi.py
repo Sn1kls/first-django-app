cce9f26949d7e125a2264948c10fffb88f625a40
Sn1kls <macks.chigrin@gmail.com>
2024-11-28 15:08:30 +0200

    change file

--- first_django_app/asgi.py
+++ first_django_app/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "first-django-app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "first_django_app.settings")
 
 application = get_asgi_application()
